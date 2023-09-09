07dbc7f8fbe2f1431613107b27864601723375bf
prathu12 <113411774+prathu12@users.noreply.github.com>
2023-09-09 11:05:35 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+window.addEventListener("mousemove",function(details){
+    let rect = document.querySelector("#rectangle");
+    let xval = gsap.utils.mapRange(0, window.innerWidth, 100+ rect.getBoundingClientRect().width/2, window.innerWidth - (100 + rect.getBoundingClientRect().width/2), details.clientX);
+    gsap.to('#rectangle',{
+        left:xval,
+        ease:Power3
+    })
+});
